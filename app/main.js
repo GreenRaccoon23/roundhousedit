db46b74df450b4c638f78a8ac9c816e43d3c55a2
GreenRaccoon23 <GreenRaccoon23@gmail.com>
2015-12-30 14:17:05 -0600

    Fixed main.js to require only server/index.js

--- app/main.js
+++ app/main.js
@@ -1,9 +1,1 @@
-// Require language extensions BEFORE anything else
-require('../ext');
-var m = require('mithril');
-
-var app = require('./components/app');
-
-m.route(document.getElementById('app'), '/', {
-  '/': app
-});
+require('./server/index.js');
