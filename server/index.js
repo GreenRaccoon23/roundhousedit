2b5c6a1fafda857df8f474dac7ea8c9b8b316884
GreenRaccoon23 <GreenRaccoon23@gmail.com>
2016-03-01 14:20:31 -0600

    Removed commented out code

--- server/index.js
+++ server/index.js
@@ -1,5 +1,4 @@
 var Path = require('path');
-var browserify = require('browserify-middleware');
 var partials = require('express-partials');
 var bodyParser = require('body-parser');
 var express = require('express');
@@ -7,21 +6,12 @@ var app = express();
 var root = __dirname.replace(/^(.*)\/.*$/, '$1');
 console.log('root', root);
 
-// app.set('views', __dirname + '/client');
-// app.set('view engine', 'ejs');
 app.use(partials());
-// Parse JSON (uniform resource locators)
 app.use(bodyParser.json());
-// Parse forms (signup/login)
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(root + '/client'));
 
-//
-// Serve a browserified file for GET /scripts/app-bundle.js
-//
-app.get('/',
-   // browserify('./client/components/app.js'));
-function(req, res) {
+app.get('/', function(req, res) {
   res.redirect('/client/index')
 })
 
